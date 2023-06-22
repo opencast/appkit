9d503ec4857d9b38d8f653cf1998d63edef1393f
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-06-22 13:06:48 +0200

    Add ESLint linting

--- src/err.tsx
+++ src/err.tsx
@@ -19,7 +19,7 @@
  * places which we might need to change after adding a new variant.
  */
 export const assertNever = (_n: never): never =>
-    bug("`assertNever` call was reached, that's a soundness hole in the typesystem :(");
+  bug("`assertNever` call was reached, that's a soundness hole in the typesystem :(");
 
 /**
  * A custom error type that represents bugs: errors that are not expected and
@@ -27,19 +27,19 @@ export const assertNever = (_n: never): never =>
  * "world" (e.g. any input). Use the helper functions below to throw this error.
  */
 export class Bug extends Error {
-    public constructor(msg: string) {
-        super(`${msg} (this is a bug in this application)`);
-        this.name = "Bug";
-    }
+  public constructor(msg: string) {
+    super(`${msg} (this is a bug in this application)`);
+    this.name = "Bug";
+  }
 }
 
 /** Throws a `Bug` error. Use this function to signal a bug in the code. */
 export const bug = (msg: string): never => {
-    throw new Bug(msg);
+  throw new Bug(msg);
 };
 
 /** Like `bug`, but specifically for code paths that should be unreachable. */
 export const unreachable = (msg?: string): never => {
-    const prefix = "reached unreachable code";
-    throw new Bug(msg === undefined ? prefix : `${prefix}: ${msg}`);
+  const prefix = "reached unreachable code";
+  throw new Bug(msg === undefined ? prefix : `${prefix}: ${msg}`);
 };
