6d4278f1663113af1746fbb6165ee908012d4ccd
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-06-26 10:52:23 +0200

    Add a way to configure appkit components via context

---
+++ src/config.tsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+
+
+/** Configuration of this library. */
+export type AppkitConfig = {
+  colors: {
+    neutral0: string,
+    neutral1: string,
+    neutral2: string,
+    neutral3: string,
+    neutral4: string,
+    neutral5: string,
+    neutral6: string,
+    neutral7: string,
+    neutral8: string,
+    neutral9: string,
+
+    focus: string;
+  },
+};
+
+export const DEFAULT_APPKIT_CONFIG: AppkitConfig = {
+  colors: {
+    neutral0: "var(--color-neutral0)",
+    neutral1: "var(--color-neutral1)",
+    neutral2: "var(--color-neutral2)",
+    neutral3: "var(--color-neutral3)",
+    neutral4: "var(--color-neutral4)",
+    neutral5: "var(--color-neutral5)",
+    neutral6: "var(--color-neutral6)",
+    neutral7: "var(--color-neutral7)",
+    neutral8: "var(--color-neutral8)",
+    neutral9: "var(--color-neutral9)",
+
+    focus: "var(--color-focus)",
+  },
+};
+
+const Context = React.createContext(DEFAULT_APPKIT_CONFIG);
+
+/** Returns the current configuration for your component. */
+export const useConfig = (): AppkitConfig => {
+  return useContext(Context);
+};
+
+
+type AppkitConfigProviderProps = React.PropsWithChildren<{
+  config: AppkitConfig;
+}>;
+
+/**
+ * Allows you to set a custom configuration for appkit components that are
+ * (indirect) children of this component.
+ */
+export const AppkitConfigProvider: React.FC<AppkitConfigProviderProps> = ({ config, children }) => (
+  <Context.Provider value={config}>{children}</Context.Provider>
+);
