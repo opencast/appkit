061d4817f5c78fe879db5f87f4458581171d54ed
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-07-06 14:03:25 +0200

    Make second parameter of `focusStyle` optional and add docs

--- src/index.tsx
+++ src/index.tsx
@@ -17,10 +17,31 @@ export * from "./util";
  * outline's width. Otherwise is also possible to declare an additional offset
  * (usually 1) for elements with a similar color to the outline to make it
  * stand out more.
+ *
+ * In your application, you likely want to define your own wrapper for this with
+ * a fixed first parameter:
+ *
+ * ```
+ * import { focusStyle as appkitFocusStyle } from "@opencast/appkit";
+ *
+ * export const focusStyle = (options?: Parameters<typeof appkitFocusStyle>[1]) =>
+ *   appkitFocusStyle(MY_APPKIT_CONFIG, options);
+ * ```
+ *
+ * Where `MY_APPKIT_CONFIG` is what you pass to the `AppkitConfigProvider`, or
+ * `DEFAULT_CONFIG` if you don't override the config.
  */
-export const focusStyle = (config: AppkitConfig, { width = 2.5, inset = false, offset = 0 }) => ({
-  "&:focus-visible": {
-    outline: `${width}px solid ${config.colors.focus}`,
-    outlineOffset: `${inset ? -width : offset}px`,
-  },
-} as const);
+export const focusStyle = (
+  config: AppkitConfig,
+  options?: { width?: number; inset?: boolean; offset?: number },
+) => {
+  const width = options?.width ?? 2.5;
+  const inset = options?.inset ?? false;
+  const offset = options?.offset ?? 0;
+  return {
+    "&:focus-visible": {
+      outline: `${width}px solid ${config.colors.focus}`,
+      outlineOffset: `${inset ? -width : offset}px`,
+    }
+  } as const;
+};
